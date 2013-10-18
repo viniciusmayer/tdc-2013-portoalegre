5bfe570edb83c01d92af2e1ce4137b7959fdadb1
Vinicius Mayer <viniciusmayer@gmail.com>
2013-10-18 12:01:49 -0300

    alteração do nome do projeto, de tdc-portoalegre-2013 para tdc-2013-portoalegre

---
+++ src/tdc/2013/portoalegre/scripts/setup.py
@@ -0,0 +1,16 @@
+'''
+Created on Oct 18, 2013
+
+@author: eleonorvinicius
+'''
+file = open("../../../../../tdc/2013/portoalegre/db/changelog/sql/setup.sql", "w+")
+for i in range(10):
+    file.write("insert into pessoa (nome, cpf) values ('{}', {});\n".format("nome {}".format(i), i))
+
+k = 0
+for i in range(10):
+    for j in range(10):
+        k = k + 1
+        file.write("insert into mensagem (texto, pessoa_id) values ('{}', (select id from pessoa where cpf = '{}'));\n".format("texto {}", j).format(k))
+
+file.close()
